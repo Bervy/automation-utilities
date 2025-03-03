5a793f08278e9c3b14d0acac80101ea3ecbe105a
Vladislav Osipov <vladshelkovo@yandex.ru>
2025-03-03 09:57:42 +0300

    Мой первый коммит.

---
+++ src/main/java/com/example/automation/utilities/AutomationUtilitiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.automation.utilities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationUtilitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationUtilitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/automation/utilities/AutomationUtilitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.automation.utilities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationUtilitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
